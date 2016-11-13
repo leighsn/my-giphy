ecc5b12fd7ec19f1a875ab19c225a917a65a4a13
digitalbuddha <leighscherrer@gmail.com>
2016-11-13 15:03:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Make a Giphy Clone
+
+This app was configured with create-react-app. Check out [the documentation](create-react-app-readme.md).
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '../public/css/Bootstrap-Cosmo.css'
+import '../public/css/App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      App will go here!
+      </div>
+    );
+  }
+}
+
+export default App;
